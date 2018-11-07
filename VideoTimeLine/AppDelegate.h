2bf6012b6333735a8298da646e305d54abf55b7b
Space Zhong <spacezhong@spacedemacbook-pro.local>
2018-11-07 09:19:56 +0800

    Initial Commit

---
+++ VideoTimeLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoTimeLine
+//
+//  Created by Space Zhong on 2018/11/7.
+//  Copyright © 2018年 Space Zhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoTimeLine/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoTimeLine
+//
+//  Created by Space Zhong on 2018/11/7.
+//  Copyright © 2018年 Space Zhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
